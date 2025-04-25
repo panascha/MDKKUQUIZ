11e0b1581012af86b3d50961c7f68ec7446364b4
Arthur <thanagorn2005@gmail.com>
2025-04-25 21:22:21 +0700

    change to home page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,39 +1,12 @@
 import React from 'react';
-import Link from 'next/link';
 
-const Home = () => {
+export default function Home() {
     return (
-        <>
-            <div className="container mx-auto flex flex-col items-center justify-center min-h-screen gap-4">
-                <div className="flex items-center justify-center m-8">
-                    <h1 className="text-4xl font-bold text-center">Welcome to the Main Page</h1>
-                </div>
-                <div className="flex items-center justify-center m-4">
-                    <p className="text-lg text-gray-600 text-center">Navigate through the sections below to explore more.</p>
-                </div>
-
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-4 w-full left-0 right-0 top-0 bottom-0 p-4">
-                    <Link href="/subjects" className="box bg-black text-white text-center text-xl p-6 rounded shadow-md hover:bg-blue-600 transition duration-300 ease-in-out">
-                        Subject
-                    </Link>
-                    <Link href="/atlas" className="box bg-black text-white text-center text-xl p-6 rounded shadow-md hover:bg-green-600 transition duration-300 ease-in-out">
-                        Atlas
-                    </Link>
-                    <Link href="/keyword" className="box bg-black text-white text-center text-xl p-6 rounded shadow-md hover:bg-yellow-600 transition duration-300 ease-in-out">
-                        Keyword
-                    </Link>
-                    <Link href="/report" className="box bg-black text-white text-center text-xl p-6 rounded shadow-md hover:bg-red-600 transition duration-300 ease-in-out">
-                        Report
-                    </Link>
-                </div>
-                <div className='grid grid-cols-1 gap-4 left-0 right-0 top-0 bottom-0 p-2 w-1/2'>
-                    <Link href="/admin" className="box bg-black text-white text-center text-xl p-6 rounded shadow-md hover:bg-red-800 transition duration-300 ease-in-out">
-                        Admin
-                    </Link>
-                </div>
-            </div>
-        </>
+        <div>
+            <h1>Welcome to Our Website!</h1>
+            <p>
+                This is the home page of our amazing project. Explore and enjoy the features we have to offer!
+            </p>
+        </div>
     );
 };
-
-export default Home;
