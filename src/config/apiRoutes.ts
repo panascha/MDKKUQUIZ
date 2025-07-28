1ce2cf00c3910425f6732697d5460ad7075d8d28
ArthurEZ <thanagorn2005@gmail.com>
2025-07-28 18:11:34 +0700

    Update BACKEND_URL to production and comment out old URL

--- src/config/apiRoutes.ts
+++ src/config/apiRoutes.ts
@@ -1,4 +1,5 @@
-export const BACKEND_URL = "https://medquizbackend-101913825665.asia-southeast1.run.app";
+//export const BACKEND_URL = "https://medquizbackend-101913825665.asia-southeast1.run.app";
+export const BACKEND_URL = "https://medquizbackend-production-04fe.up.railway.app";
 //export const BACKEND_URL = "http://localhost:5001";
 const BackendRootRoutes = `${BACKEND_URL}/api/v1`;
 
