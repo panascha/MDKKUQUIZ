bb59b50d2ad487de8c0aa76d1e0c0669e384ac03
ArthurEZ <thanagorn2005@gmail.com>
2025-07-31 16:22:16 +0700

    refactor: Remove unused quiz layout and problem page components

--- src/app/home/[subjectID]/quiz/layout.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-
-import React from 'react'
-import { QuizProvider } from '../../../../context/quiz'
-
-export default function QuizLayout({ children }: { children: React.ReactNode }) {
-  return <QuizProvider>{children}</QuizProvider>
-}
