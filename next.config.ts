05fc226c170eb06921cdbcb363f06c1351f01a3a
Panas Changtrakul <panasch@kkumail.com>
2026-01-27 13:24:43 +0700

    test SSH VScode save

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,9 @@ const nextConfig: NextConfig = {
       { protocol: 'https', hostname: 'medquizbackend-101913825665.asia-southeast1.run.app' },
       { protocol: 'https', hostname: 'medquizbackend-production-04fe.up.railway.app' },
       { protocol: 'https', hostname: 'media1.tenor.com' },
-
+      { protocol: 'http', hostname: '10.101.111.139' , port: '5000', pathname: '/**'},
+      { protocol: 'https', hostname: '10.101.111.139' },
+      { protocol: 'https', hostname: 'mseb.md.kku.ac.th' },
     ],
   },
 };
